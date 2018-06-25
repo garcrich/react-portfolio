52aa56dc5d4ca3f249f34b351d2f635d09c1d60c
Ricky Garcia <rg3646@outlook.com>
2018-06-24 21:18:44 -0600

    fixes button styling for 404

--- client/src/components/pages/ErrorPage.js
+++ client/src/components/pages/ErrorPage.js
@@ -20,10 +20,8 @@ export default class ErrorPage extends React.Component {
                     <div className="error-div" style={{display : this.state.display ? 'block' : 'none'}}>
                         <h2 className="error-div__header text-center ">404</h2>
                         <h2 className="error-div__sub-header text-center ">Oh no! Page not found.</h2>
-                        <Link to={`/`} className="text-center hero__content__button" onClick={this.displayNone}>
-                        <button>
+                        <Link to={`/`} onClick={this.displayNone}>
                         Return to Home
-                        </button>
                         </Link>
 
                     </div>
