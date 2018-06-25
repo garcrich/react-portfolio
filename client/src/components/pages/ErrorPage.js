04855f97d703e7e37a2d8ebf45e8284c934d0f1c
Ricky Garcia <rg3646@outlook.com>
2018-06-24 21:25:45 -0600

    fixes 404 error link

--- client/src/components/pages/ErrorPage.js
+++ client/src/components/pages/ErrorPage.js
@@ -20,7 +20,7 @@ export default class ErrorPage extends React.Component {
                     <div className="error-div" style={{display : this.state.display ? 'block' : 'none'}}>
                         <h2 className="error-div__header text-center ">404</h2>
                         <h2 className="error-div__sub-header text-center ">Oh no! Page not found.</h2>
-                        <Link to={`/`} onClick={this.displayNone}>
+                        <Link to={`/`} className="btn btn--white center-content" onClick={this.displayNone}>
                         Return to Home
                         </Link>
 
